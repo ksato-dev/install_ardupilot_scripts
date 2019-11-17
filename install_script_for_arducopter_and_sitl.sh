398c3a3985dcb24178b5fa1853e7aff005a4accb
ksato-dev <k-sato@smartrobotics.jp>
2019-11-17 11:08:47 +0900

    modify script.

--- install_script_for_arducopter_and_sitl.sh
+++ install_script_for_arducopter_and_sitl.sh
@@ -21,7 +21,7 @@ sudo adduser $your_name dialout
 git clone https://github.com/ksato-dev/ardupilot.git   # clone
 cd ardupilot   # ディレクトリ（フォルダ）変更
 git submodule update --init --recursive   # サブモジュールのアップデート
-git checkout -b create_new_flight_mode   # 開発用ブランチにチェックアウト
+git checkout -b create_new_flight_mode origin/create_new_flight_mode   # 開発用ブランチにチェックアウト
 sudo ./Tools/enviroment_install/install-prereqs-ubuntu.sh   # Ubuntu 用インストールスクリプト実行
 ./waf configure
 ./Tools/autotest/sim_vehicle.py -v ArduCopter --console --map   # SITL 実行 & MAVProxy 実行（コンソール＋ map オプション）
